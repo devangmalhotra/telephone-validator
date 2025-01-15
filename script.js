b8b2546463f6e795ef77b03b0fd91d6a11ce8da9
Devang Malhotra <119973585+devangmalhotra@users.noreply.github.com>
2025-01-15 10:59:07 -0500

    finished regex pattern to find valid phone number

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const resultsDiv = document.getElementById("results-div");
 
 const validPhoneNumber = () => {
   let userInput = document.getElementById("user-input");
-  const regex = "";
+  const countryCodeRegex = '^(1 ?)?';
+  const areaCodeRegex = '(\\([0-9]{3}\\)|[0-9]{3})';
+  const spaceOrDashRegex = '[ -]?';
+  const numberAfterAreaCodeAndFirstDashRegex = '[0-9]{3}[ -]?[0-9]{4}$'; // since ending, don't need capture group
+  const finalPhoneNumberRegex = new RegExp(`${countryCodeRegex}${areaCodeRegex}${spaceOrDashRegex}${numberAfterAreaCodeAndFirstDashRegex}`);
+  console.log(finalPhoneNumberRegex);
   
   if (userInput.value.length === 0) {
     alert("Please provide a phone number");
